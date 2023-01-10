8df52bf4a670a1f1edb81bcaa2df9a623b308fb0
mboschini <boschini.mariano@gmail.com>
2023-01-10 00:43:44 -0300

    Revert "Esto es una locura"
    This reverts commit 57daec6d1eca396e615a4ea377ff7e5290eaa226.

--- Assets/Scripts/ManagerWS/Entities/PlayerActions.cs
+++
@@ -1,9 +0,0 @@
-
-
-public class PlayerActions 
-{
-    public string ConnectionUUID {get;set;}
-    public string Action {get;set;}
-    public float MouseX {get; set;}
-    public float MouseY {get; set;}
-}
